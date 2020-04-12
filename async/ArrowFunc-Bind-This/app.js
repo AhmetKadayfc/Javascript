003f6e88077da144e7051662128c8228665aa595
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2020-04-12 11:22:29 +0300

    Callback ile ajax işlemmeri

--- async/ArrowFunc-Bind-This/app.js
+++ async/ArrowFunc-Bind-This/app.js
@@ -1,13 +1,39 @@
-const person = {
-    age: 25,
-
-    // tellAge: function () {
-    //     console.log(this)
-    //     console.log(this.age)
-    // }.bind(this)
-    tellAge: () => {
-        console.log(this)
-        console.log(this.age)
+// const person = {
+//     age: 25,
+// tellAge: function () {
+//     console.log(this)
+//     console.log(this.age)
+// }.bind(this)
+// tellAge: () => {
+//     console.log(this)
+//     console.log(this.age)
+// }
+// }
+// person.tellAge()
+// console.log(this)
+class Request {
+    constructor() {
+        this.xhr = new XMLHttpRequest()
     }
-}
-person.tellAge()
+
+    get(url, callback) {
+        this.xhr.open('GET', url)
+
+        this.xhr.onload = () => {
+            if (this.xhr.status == 200) {
+                callback(null, JSON.parse(this.xhr.responseText))
+            } else {
+                callback("Yanlış giden bir şeyler var!", null)
+            }
+        }
+        this.xhr.send()
+    }
+} const request = new Request()
+
+request.get('https://jsonplaceholder.typicode.com/albums/67', function (err, response) {
+    if (err === null) {
+        console.log(response)
+    } else {
+        console.log(err)
+    }
+})
