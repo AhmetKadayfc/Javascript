c6e69389a43986f17d6be94cabe6946707f7aea5
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2020-04-12 16:55:20 +0300

    fetch ile GET Request

--- async/Fetch/app.js
+++ async/Fetch/app.js
@@ -1,23 +1,42 @@
-function getTextFilet() {
-    fetch('text.txt')
-        .then(respone => respone.text())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
+// function getTextFilet() {
+//     fetch('text.txt')
+//         .then(respone => respone.text())
+//         .then(data => console.log(data))
+//         .catch(err => console.log(err))
+// }
+
+// function getJsonFile() {
+//     fetch('example.json')
+//         .then(data => data.json())
+//         .then(data => console.log(data))
+//         .catch(err => console.log(err))
+// }
+// function getExternalFile() {
+//     fetch('https://api.exchangeratesapi.io/latest')
+//         .then(data => data.json())
+//         .then(data => console.log(data.rates.TRY))
+//         .catch(err => console.log(err))
+// }
+
+// getTextFilet()
+// getJsonFile()
+// getExternalFile()
+
+// FETCH GET İŞLEMLERİ
+
+class Request {
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(data => data.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        })
+    }
 }
 
-function getJsonFile() {
-    fetch('example.json')
-        .then(data => data.json())
-        .then(data => console.log(data))
-        .catch(err => console.log(err))
-}
-function getExternalFile() {
-    fetch('https://api.exchangeratesapi.io/latest')
-        .then(data => data.json())
-        .then(data => console.log(data.rates.TRY))
-        .catch(err => console.log(err))
-}
-
-getTextFilet()
-getJsonFile()
-getExternalFile()
+const reques = new Request()
+let albums
+reques.get('https://jsonplaceholder.typicode.com/albums')
+    .then(albums => console.log(albums))
+    .catch(err => console.log(err))
