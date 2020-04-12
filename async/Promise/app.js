1b681f8efeb945e44fa792fad867d431936a212f
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2020-04-12 15:05:28 +0300

    chain

--- async/Promise/app.js
+++ async/Promise/app.js
@@ -12,6 +12,26 @@ function getData(data) { // Promise objesini döndüren fonksiyon
     })
 }
 
-getData(23)
+function addTwo(number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof number === 'number') {
+                resolve(number + 2)
+            } else {
+                reject("Numner türünde bir veri giriniz!")
+            }
+        },4000)
+    })
+}
+
+getData("hola")
     .then((response) => console.log(response))
     .catch((err) => console.error(err))
+
+addTwo(25)
+    .then(response => {
+        console.log(response)
+        return response + 2
+    })
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
