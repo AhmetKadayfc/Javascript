2607dffc3254d5d21d4044e4d83e3b46dce20dcc
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2020-04-12 13:09:58 +0300

    callback ile ajax put işlemleri

--- async/ArrowFunc-Bind-This/app.js
+++ async/ArrowFunc-Bind-This/app.js
@@ -42,6 +42,21 @@ class Request {
         }
         this.xhr.send(JSON.stringify(data))
     }
+    put(url, data, callback) {
+        this.xhr.open('PUT', url)
+        this.xhr.setRequestHeader("Content-type", "application/json")
+        this.xhr.onprogress = () => {
+            console.log('İşlem gerçekleşiyor...')
+        }
+        this.xhr.onload = () => {
+            if (this.xhr.status == 200) {
+                callback(null, this.xhr.responseText)
+            } else {
+                callback('Yanlış giden bir şeyler var!', null)
+            }
+        }
+        this.xhr.send(JSON.stringify(data))
+    }
 }
 
 const request = new Request()
@@ -54,7 +69,11 @@ const request = new Request()
 //     }
 // })
 
-request.post('https://jsonplaceholder.typicode.com/albums', { userId: 2, title: 'Tom' }, function
-    (err, response) {
+// request.post('https://jsonplaceholder.typicode.com/albums', { userId: 2, title: 'Tom' }, function
+//     (err, response) {
+//     err === null ? console.log(response) : console.log(err)
+// })
+
+request.put('https://jsonplaceholder.typicode.com/albums/56', { userId: 43, title: 'Ac/DC' }, function (err, response) {
     err === null ? console.log(response) : console.log(err)
 })
